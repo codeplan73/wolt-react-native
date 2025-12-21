28e7f2b83d16968cadee49e72dc37cfe7cd5d6d0
Emmanuel Omonzebaguan <omonzebaguan.grn@gmail.com>
2025-12-21 20:30:45 +0100

    updated modal layout

--- app/(app)/(auth)/_layout.tsx
+++ app/(app)/(auth)/_layout.tsx
@@ -11,7 +11,7 @@ const Layout = () => {
     <Stack>
       {/* <Stack.Screen name="index" options={{ title: "Feed" }} /> */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)/map" options={{ headerShown: false }} />
+      <Stack.Screen name="(modal)/map" options={{ headerShown: false }} />
       {/* <Stack.Screen
         name="(modal)/(restaurant)/[id]"
         options={{
